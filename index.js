969d81ff78c0144230f498e3462b45a0a997ece3
gokulajmech <85476372+gokulajmech@users.noreply.github.com>
2021-12-15 09:09:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let box=document.querySelector('.mailbody');
+
+function compose(){
+    box.innerHTML=
+ `  
+    <div class="compose-head fonts">
+    <div class="compose-topbar">New Message</div>
+    <input type="text" placeholder="TO" class="compose-border1"></input>
+    <input type="text" placeholder="Recipient" class="compose-border1"></input>
+    <div class="textarea">
+    <textarea name="compose" id="compose" cols="50" rows="20"></textarea>
+    </div>
+    <button>sent mail</button> 
+    `
+    
+};
